1d339af5b981d340844bfbacf59110f50cfff728
Johan Palacios <johanpalacios.rivera@gmail.com>
2024-10-13 22:59:53 -0600

    feat: initial backend

---
+++ basedatosback/main.py
@@ -0,0 +1,20 @@
+import pyodbc
+
+SERVER = "localhost"
+DATABASE = "master"
+USERNAME = "sa"
+PASSWORD = "fakePassw0rd"
+PORT = "1433"
+
+# connectionString = f"DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={SERVER};DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD}"
+
+connectionString = f"DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={SERVER};DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD};TrustServerCertificate=Yes"
+
+try:
+    conn = pyodbc.connect(connectionString)
+    print("Conectado")
+except Exception as e:
+    print("No conecto\n", e)
+
+
+#
