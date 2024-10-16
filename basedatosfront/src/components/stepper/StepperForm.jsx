c9710ae230fb1581ba1e225faa274828d372f456
Johan Palacios <johanpalacios.rivera@gmail.com>
2024-10-15 20:14:23 -0600

    feat: Stepper Form

---
+++ basedatosfront/src/components/stepper/StepperForm.jsx
@@ -0,0 +1,57 @@
+import {
+  Step,
+  StepDescription,
+  StepIcon,
+  StepIndicator,
+  StepNumber,
+  StepSeparator,
+  StepStatus,
+  StepTitle,
+  Stepper,
+  useSteps,
+  Box,
+} from '@chakra-ui/react'
+import { PropTypes } from 'prop-types'
+
+// onst steps = [
+//   { title: 'First', description: 'Contact Info' },
+//   { title: 'Second', description: 'Date & Time' },
+//   { title: 'Third', description: 'Select Rooms' },
+// ]
+
+const StepperForm =  ({steps}) => {
+  const { activeStep } = useSteps({
+    index: 1,
+    count: steps.length,
+  })
+
+  return (
+    <Stepper index={activeStep} marginBottom={16}>
+      {steps.map((step, index) => (
+        <Step key={index}>
+          <StepIndicator>
+            <StepStatus
+              complete={<StepIcon />}
+              incomplete={<StepNumber />}
+              active={<StepNumber />}
+            />
+          </StepIndicator>
+
+          <Box flexShrink='0'>
+            <StepTitle>{step.title}</StepTitle>
+            <StepDescription>{step.description}</StepDescription>
+          </Box>
+
+          <StepSeparator />
+        </Step>
+      ))}
+    </Stepper>
+  )
+}
+
+StepperForm.propTypes = {
+  steps: PropTypes.object
+}
+
+export default StepperForm
+
