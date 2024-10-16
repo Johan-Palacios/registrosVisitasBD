973a2dd36c3a65a69f65b5945806827281665d22
Johan Palacios <johanpalacios.rivera@gmail.com>
2024-10-15 20:14:15 -0600

    feat: add FormBase

---
+++ basedatosfront/src/components/forms/FormBase.jsx
@@ -0,0 +1,19 @@
+import { Box, Heading, Stack } from "@chakra-ui/react"
+import { PropTypes } from 'prop-types'
+const FormBase = ({ nameForm, children }) => {
+  return (
+    <>
+      <Heading marginBottom={16}>{nameForm}</Heading>
+      <Box as="form">
+        {children}
+      </Box>
+    </>
+  )
+}
+
+FormBase.propTypes = {
+  nameForm: PropTypes.string,
+  children: PropTypes.element
+}
+
+export default FormBase
