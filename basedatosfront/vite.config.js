3eaa1b70a65e0908615ce1a1de2a5f449b3a5cc9
Johan Palacios <johanpalacios.rivera@gmail.com>
2024-10-15 20:14:38 -0600

    feat: add alias

--- basedatosfront/vite.config.js
+++ basedatosfront/vite.config.js
@@ -1,7 +1,16 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite'
+import path from 'path'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+    }
+  },
 })
