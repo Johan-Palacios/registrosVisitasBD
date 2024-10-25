9d508ea18f96e7b20267b35aa6d979cdbca15f36
Johan Palacios <johanpalacios.rivera@gmail.com>
2024-10-25 01:15:34 -0600

    feat: add Nuevo Tramite API Insert

--- basedatosfront/src/pages/forms/NuevoTramite.jsx
+++ basedatosfront/src/pages/forms/NuevoTramite.jsx
@@ -1,14 +1,68 @@
-import { Button, FormLabel, Input, Stack, FormControl } from '@chakra-ui/react'
+import { Button, FormLabel, Input, Stack, FormControl, useToast } from '@chakra-ui/react'
 import FormBase from '@components/forms/FormBase.jsx'
+import { useState } from 'react'
 
 const NuevoTramite = () => {
+  const toast = useToast()
+  const [tramite, setTramite] = useState()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const data = { tramite }
+    fetchInsertTramite(data)
+    e.target.reset()
+  }
+
+  const fetchInsertTramite = async (data) => {
+    const token = localStorage.getItem('token')
+
+    const insertToast = toast({
+      title: 'Conectando con el servidor',
+      description: 'Cargando...',
+      status: 'loading',
+    })
+
+    try {
+      const response = await fetch('http://localhost:8000/insertar-tramite', {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+      })
+
+      toast.close(insertToast)
+      if (response.ok) {
+        toast({
+          title: 'Trámite Guardado',
+          description: 'Inserción Exitosa',
+          status: 'success',
+        })
+      }
+      if (!response.ok) {
+        toast({
+          title: 'Error',
+          description: 'No es posible guardar el Trámite',
+          status: 'error',
+        })
+      }
+    } catch (error) {
+      toast({
+        title: 'Error',
+        description: 'No se pudo establecer conexión con el servidor',
+        status: 'error',
+      })
+    }
+  }
+
   return (
     <>
-      <FormBase nameForm='Nuevo Tramite'>
+      <FormBase nameForm='Nuevo Tramite' onSubmit={handleSubmit}>
         <FormControl>
           <Stack spacing={4}>
             <FormLabel>Ingresa un Nuevo Tramite</FormLabel>
-            <Input type='text' placeholder='Ejemplo: Tramite de XX para XX' required />
+            <Input type='text' placeholder='Ejemplo: Tramite de XX para XX' required onChange={(e) => { setTramite(e.target.value) }} />
             <Button
               mt={4}
               colorScheme='teal'
