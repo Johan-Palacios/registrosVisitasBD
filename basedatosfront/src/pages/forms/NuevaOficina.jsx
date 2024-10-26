a4a09663c6e9eb91e55c3a504067245e0723ab61
Johan Palacios <johanpalacios.rivera@gmail.com>
2024-10-26 17:13:37 -0600

    feat: Form with NuevaOficina Complete

--- basedatosfront/src/pages/forms/NuevaOficina.jsx
+++ basedatosfront/src/pages/forms/NuevaOficina.jsx
@@ -1,31 +1,57 @@
 import useGetFetch from '@/hooks/useGetFetch'
-import { Button, FormLabel, Input, Stack, FormControl, Select } from '@chakra-ui/react'
+import fetchInsert from '@/utils/fetch/fetchInsert'
+import { Button, FormLabel, Input, Stack, FormControl, Select, useToast } from '@chakra-ui/react'
 import FormBase from '@components/forms/FormBase.jsx'
+import { useState } from 'react'
 
 const NuevoOficina = () => {
-  const {data, loading, error} = useGetFetch('http://localhost:8000/edificios')
-  console.log(data)
+  const toast = useToast()
+  const { data, loading, error } = useGetFetch('http://localhost:8000/edificios')
+  const [idEdificio, setidEdificio] = useState()
+  const [oficina, setOficina] = useState()
+  const [numeroOficina, setNumeroOficina] = useState()
+
+  const handleSelectChange = (event) => {
+    setidEdificio(event.target.value)
+  }
+
+  const fetchInsertOficina = async (data) => {
+    await fetchInsert('http://localhost:8000/insertar-oficina',
+      data,
+      toast,
+      'No se pudo insertar la Oficina\nPorque ya Existe', 'Oficina Guardada')
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const data = { idEdificio, oficina, numeroOficina }
+    fetchInsertOficina(data)
+    e.target.reset()
+  }
 
   return (
     <>
-      <FormBase nameForm='Nueva Oficina'>
+      <FormBase nameForm='Nueva Oficina' onSubmit={handleSubmit}>
         <FormControl>
           <Stack spacing={4}>
-            <FormLabel>Ingresa un Nueva Oficina</FormLabel>
-            <Input type='text' placeholder='Ejemplo: Oficina XX' required />
 
-            {/* {loading? } */}
-            <FormLabel>Seleccione Edificio</FormLabel>
-            <Select placeholder='Ningun parametro Seleccionado' required marginBottom={2}>
-              {/* {availableInterestParam.map(({ name }) => { */}
-              {/*   return <option key={name}>{name}</option> */}
-              <option>Edificio 1 Ejemplo</option>
-              <option>Edificio 2 Ejemplo</option>
-              <option>Edificio 3 Ejemplo</option>
-              <option>Edificio 4 Ejemplo</option>
-              <option>Edificio 5 Ejemplo</option>
-              {/* })} */}
-            </Select>
+            <FormLabel>Ingresa un Nueva Oficina</FormLabel>
+            <Input type='text' placeholder='Ejemplo: Oficina XX' required maxLength={50} onChange={(e) => setOficina(e.target.value)} />
+            <FormLabel>Ingresa un Numero Oficina</FormLabel>
+            <Input type='number' placeholder='Ejemplo: 1' required onChange={(e) => setNumeroOficina(e.target.valueAsNumber)} />
+
+            {!loading
+
+              ? <>
+                <FormLabel>Seleccione Edificio</FormLabel>
+                <Select placeholder='Ningun Edificio' required marginBottom={2} onChange={handleSelectChange}>
+                  {data.map(({ idEdificioNumber, idEdificio, edificio }) => {
+                    return <option key={idEdificioNumber} value={idEdificioNumber}>{`${idEdificio} - ${edificio}`}</option>
+                  })}
+                </Select>
+                {/* eslint-disable-next-line @stylistic/jsx-indent */}
+                </>
+              : <></>}
 
             <Button
               mt={4}
