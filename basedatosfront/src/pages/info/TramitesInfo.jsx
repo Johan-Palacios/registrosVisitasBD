90d69001560c4f0c0a00e4bdef6ed75746eed0e0
Johan Palacios <johanpalacios.rivera@gmail.com>
2024-10-29 19:11:17 -0600

    feat: add tramites Info with table

---
+++ basedatosfront/src/pages/info/TramitesInfo.jsx
@@ -0,0 +1,20 @@
+import TableInfo from '@/components/userInteraction/info/TableInfo'
+import useGetFetch from '@/hooks/useGetFetch'
+import { Skeleton } from '@chakra-ui/react'
+
+const FuncionariosInfo = () => {
+  const { data: tramites, loading: loadingTramites } = useGetFetch('http://localhost:8000/tramites')
+  return (
+    <>
+      <Skeleton isLoaded={!loadingTramites} fadeDuration={0.4}>
+        {!loadingTramites
+          ? <TableInfo data={tramites} primaryKey='idTramiteNumber' />
+          : <></>}
+      </Skeleton>
+
+    </>
+
+  )
+}
+
+export default FuncionariosInfo
